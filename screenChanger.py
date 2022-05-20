6db1dd4a596b2c8fd8d3ddad89859fa9d5563e17
Crunch <91413622+CrunchV1@users.noreply.github.com>
2022-05-20 14:28:12 +0200

    Update screenChanger.py

--- screenChanger.py
+++ screenChanger.py
@@ -33,3 +33,5 @@ while True:
         if(file == "clock" and oude != "clock"):
             oude = "clock"
             driver.get("file:///opt/mycroft/skills/X-man-homescreen-css/clock.html")
+        else:
+            driver.get('file:///opt/mycroft/skills/X-man-homescreen-css/hmm.html')
